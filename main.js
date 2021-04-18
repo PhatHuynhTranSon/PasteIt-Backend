ca7f89e253ffe61c0fabf6540d4f5a46b4e19dc6
PhatTranSon <phatranson1@gmail.com>
2021-04-18 15:56:15 -0700

    Added basic development tools

--- main.js
+++ main.js
@@ -9,7 +9,10 @@ const app = express();
 
 //Add body-parser middleware
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ urlextended: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//Test endpoint
+app.get("/", (req, resp) => resp.end("Hello world yo"));
 
 //Start application
 app.listen(process.env.PORT, () => console.log("Application is running"));
